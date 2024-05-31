e2b15fdf9b07b3a9f204e4a478494a5f2ef672b0
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-05-31 18:43:54 +0200

    add: activation functions

---
+++ src/nn/activation.rs
@@ -0,0 +1,54 @@
+use std::f32::consts::E;
+
+/// Step activation function
+/// 
+/// Implemented from the function: <https://en.wikipedia.org/wiki/Step_function>
+pub fn step(x:f32) -> f32{
+    if x >= 0.0{
+        return 1.0
+    }
+    return 0.0
+}
+
+/// Sigmoid activation function
+/// 
+/// Implemented from the function: <https://en.wikipedia.org/wiki/Sigmoid_function>
+pub fn sigmoid(x:f32) -> f32{
+    return 1.0 / (1.0 + E.powf(-x));
+}
+
+
+/// ReLu activation function
+/// 
+/// Implemented from the function: <https://en.wikipedia.org/wiki/Rectifier_(neural_networks)>
+pub fn relu(x:f32) -> f32{
+    if x > 0.0{
+        return x;
+    }
+    return 0.0
+}
+
+/// Softplus activation function
+/// 
+/// Implemented from the function: <https://en.wikipedia.org/wiki/Activation_function>
+pub fn softplus(x:f32) ->f32{
+    return (1.0 +  E.powf(x)).ln()
+}
+
+
+/// ELU activation function
+/// 
+/// Implemented from the function: <https://en.wikipedia.org/wiki/Activation_function>
+pub fn elu(x:f32, alpha:f32) -> f32{
+    if x > 0.0{
+        return x;
+    }
+    return alpha*(E.powf(x) - 1.0);
+}
+
+/// Softmax activation function
+/// 
+/// Implemented from the function: <https://en.wikipedia.org/wiki/Softmax_function>
+pub fn softmax(x:f32) -> f32{
+    unimplemented!()
+}
---
+++ tests/test_activation.rs
@@ -0,0 +1,44 @@
+#[cfg(test)]
+mod tests {
+    use rustic_ml::nn::activation::*;
+
+    #[test]
+    fn test_activation_step(){
+        assert_eq!(step(-3.4), 0.0);
+        assert_eq!(step(3.4), 1.0);
+        assert_eq!(step(0.0), 1.0)
+    }
+
+    #[test]
+    fn test_activation_sigmoid(){
+        assert_eq!(sigmoid(0.0), 0.5);
+        assert_eq!(sigmoid(-6.0), 0.0024726237);
+        assert_eq!(sigmoid(6.0), 0.9975274);
+    }
+
+    #[test]
+    fn test_activation_relu(){
+        assert_eq!(relu(0.0), 0.0);
+        assert_eq!(relu(1.12), 1.12);
+        assert_eq!(relu(-0.123), 0.0);
+
+    }
+
+    #[test]
+    fn test_activation_softplus(){
+        assert_eq!(softplus(-6.0), 0.002475652);
+        assert_eq!(softplus(0.0), 0.6931472);
+        assert_eq!(softplus(1.0), 1.3132616);
+        assert_eq!(softplus(1.0), 1.3132616);
+        assert_eq!(softplus(6.0), 6.0024753);
+    }
+
+    #[test]
+    fn test_activation_elu(){
+        assert_eq!(elu(6.0, 1.0), 6.0);
+        assert_eq!(elu(1.0, 1.0), 1.0);
+        assert_eq!(elu(0.0, 1.0), 0.0);
+        assert_eq!(elu(-6.0, 5.0), -4.987606);
+        assert_eq!(elu(-1.0, 5.0), -3.1606028);
+    }
+}

4922b441252a1abb1271fd04617a1959655054d7
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-09-26 20:40:51 +0200

    add: rust doc for the data utils module and the main module

--- src/data_utils/mod.rs
+++ src/data_utils/mod.rs
@@ -1,3 +1,8 @@
+//! This module provides the core functionality for working with data structures.
+//! It includes the following modules:
+//! - `datacolumn`: Handles operations related to data columns.
+//! - `dataframe`: Implements a data frame structure for data manipulation.
+//! - `matrix`: Provides functionality for matrix operations.
 pub mod datacolumn;
 pub mod dataframe;
 pub mod matrix;

2c7805b39f03bef58cdd9ecfd9616bce9ba23ee5
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-11-13 18:37:21 +0100

    refactor: matrix error to its own file

--- src/data_utils/matrix.rs
+++ src/data_utils/matrix.rs
@@ -2,70 +2,21 @@ use rand::{
     distributions::{Standard, Uniform},
     Rng,
 };
-use std::{
-    fmt::Display,
-    ops::{Range, RangeInclusive},
-};
+use std::ops::{Range, RangeInclusive};
+
+use super::matrix_error::MatrixError;
 
 /// Matrix implementation
 ///
 /// A mathematical data structure.
 /// Read more about matrices here:
 /// <https://en.wikipedia.org/wiki/Matrix_(mathematics)>
+#[derive(Debug)]
 pub struct Matrix {
     pub data: Vec<f32>,
     pub rows: usize,
     pub cols: usize,
 }
-
-/// Custom Error type for a `Matrix` operation
-#[derive(Debug, PartialEq, Eq)]
-pub enum MatrixError {
-    /// An error that occurs when a operation requires that two matrices have the exact same shape
-    ShapeMismatch {
-        /// Shape of the matrix that called the matrix operation function.
-        first_matrix_shape: String,
-
-        /// Shape of the matrix that was given in a matrix operation function.
-        second_matrix_shape: String,
-    },
-
-    /// Operation lead to division by 0
-    DivideByZero,
-
-    /// Matrix operation required a range, but the given range lead to an error,
-    IllegalRange(String),
-
-    /// Matrix multiplication with two given matrixes sizes (mxn) and (qxp)
-    /// The columns (n) must equal rows (q) => n == q
-    MatrixMultiply,
-}
-
-// For printing the error of the matrix
-impl Display for MatrixError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            MatrixError::ShapeMismatch {
-                first_matrix_shape,
-                second_matrix_shape,
-            } => {
-                write!(f, "The matrix with shape ({first_matrix_shape}) does not match the shape of the given matrix ({second_matrix_shape})")
-            }
-            MatrixError::DivideByZero => {
-                write!(f, "Given matrix or number lead to division by zero")
-            }
-            MatrixError::IllegalRange(val) => {
-                write!(f, "Illegal Range Given: {val}")
-            }
-            MatrixError::MatrixMultiply => {
-                write!(f, "Illegal to multiply the two given matrixes.")
-            }
-        }
-    }
-}
-
-// Making the MatrixError an Error
-impl std::error::Error for MatrixError {}
 
 impl Matrix {
     /// Create a new empty matrix

175cda502a25e3b3d70f8540153f7f2303488299
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-11-13 18:33:39 +0100

    add: network setup

--- src/network.rs
+++ src/network.rs
@@ -1,21 +1,38 @@
+use crate::layer::layer_trait::Layer;
+
 #[allow(dead_code)]
-pub struct Network{
+pub struct Network {
+    // Learning rate for the whole network
+    lr: f32,
 
-
-    // Represent trainable params in the neural network 
+    // Represent trainable params in the neural network
     params: usize,
 }
 
+#[allow(dead_code)]
+impl Network {
+    pub fn init() -> Self {
+        Self { lr: 0.0, params: 0 }
+    }
 
-impl Network{
-
-    pub fn init() -> Self{
-        // Set topology 
-
-
-        // Calculate and set params 
-
+    pub fn with_topology() -> Self {
         unimplemented!()
     }
-}
 
+    // Adding a layer would
+    pub fn add_layer(self, _d: impl Layer) {
+        unimplemented!()
+    }
+
+    /// Set learning rate of the network
+    ///
+    /// Uses the given learning rate to update the weights for the back propagation stage
+    pub fn learning_rate(mut self, learning_rate: f32) -> Self {
+        self.lr = learning_rate;
+        self
+    }
+
+    pub fn trainable_params(&self) -> usize {
+        self.params
+    }
+}
